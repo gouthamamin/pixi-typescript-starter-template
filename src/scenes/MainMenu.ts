42a6bf223b23dc631eea68fe8e86f9b29b09e39d
Goutham_Amin <gouthamamin@gmail.com>
2026-07-22 18:10:10 +0530

    feat: add game scene

--- src/scenes/MainMenu.ts
+++ src/scenes/MainMenu.ts
@@ -1,9 +1,12 @@
+import SceneManager from "../core/SceneManager";
 import BaseScene from "./BaseScene";
+import Game from "./Game";
 
 class MainMenu extends BaseScene{
 
-  public initialize(): void | Promise<void> {
+  public async initialize(): Promise<void> {
     console.log("MainMenu scene inti");
+    await SceneManager.changeScene(new Game());
   };
 };
 
