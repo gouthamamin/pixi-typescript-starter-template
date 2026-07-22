2d0c6a3d58186c0376e9a3a8a5064dece8417c55
Goutham_Amin <gouthamamin@gmail.com>
2026-07-22 19:04:06 +0530

    feat: add Button component

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,3 +2,11 @@ interface iAsset {
   alias: string,
   src: string
 };
+interface iButtonProps {
+  text: string;
+  width?: number;
+  height?: number;
+  backgroundColor?: string;
+  textColor?: string;
+  onClick?: () => void | Promise<void>;
+};
