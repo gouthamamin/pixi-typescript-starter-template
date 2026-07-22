eea963472996bcb9563d08ca85eba4bd6363b8e2
Goutham_Amin <gouthamamin@gmail.com>
2026-07-22 17:49:33 +0530

    feat: add main menu scene

---
+++ src/scenes/MainMenu.ts
@@ -0,0 +1,10 @@
+import BaseScene from "./BaseScene";
+
+class MainMenu extends BaseScene{
+
+  public initialize(): void | Promise<void> {
+    console.log("MainMenu scene inti");
+  };
+};
+
+export default MainMenu;
