be1f1f81cab2d29c3da30d5ff298637c7f6dc9e0
Goutham_Amin <gouthamamin@gmail.com>
2026-07-23 11:46:46 +0530

    feat: add game scene

---
+++ src/scenes/Game.ts
@@ -0,0 +1,10 @@
+import BaseScene from "./BaseScene";
+
+class Game extends BaseScene{
+
+  public initialize(): void | Promise<void> {
+    console.log("Game scene inti");
+  };
+};
+
+export default Game;
