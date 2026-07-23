b07dbb96d0e1bf2a6f00348ab4cf7b88a47226b9
Goutham_Amin <gouthamamin@gmail.com>
2026-07-23 11:45:54 +0530

    feat: add main menu scene

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -1,9 +1,12 @@
+import SceneManager from "../core/SceneManager";
 import BaseScene from "./BaseScene";
+import MainMenu from "./MainMenu";
 
-class Preloader extends BaseScene{
+class Preloader extends BaseScene {
 
-  public initialize(): void | Promise<void> {
+  public async initialize(): Promise<void> {
     console.log("Preloader scene inti");
+    await SceneManager.changeScene(new MainMenu());
   };
 };
 
